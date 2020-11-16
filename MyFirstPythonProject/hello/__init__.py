600796b04b58f02fbb0e34177fce8c70a9a8a757
wd_makangkang <313989006@qq.com>
2020-11-16 21:00:55 +0800

    提交合并多个excel的python代码

---
+++ MyFirstPythonProject/hello/__init__.py
@@ -0,0 +1,5 @@
+print("hello world")
+num1 = '11'
+num2 = '22'
+print(int(num1) + int(num2))
+print(num1 + num2)
---
+++ MyFirstPythonProject/hello/test.py
@@ -0,0 +1,12 @@
+
+def addFunction(a,b):
+    c = a + b
+    print(c)
+
+if __name__=="__main__":
+    print("执行加法***")
+    print(type(5))
+    print(type(5) == int)
+    print(type({"1":"测试"}))
+    addFunction(1,2)
+
